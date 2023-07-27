6ffe2573b367ce2ea58967fba39d0fae2c5930e9
Yasaswini38 <119612379+Yasaswini38@users.noreply.github.com>
2023-07-28 01:12:26 +0530

    Add files via upload

--- crpyield.py
+++ crpyield.py
@@ -1,60 +1,97 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+import seaborn as sns
 from sklearn.linear_model import LogisticRegression
-import seaborn as sns 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
-# Disable Matplotlib PyplotGlobalUseWarning
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-# Load the dataset
+
 data = pd.read_csv('Crop_recommendation.csv')
 
-# Sidebar Title
+
 st.sidebar.title("Crop Yield Prediction")
 
-# Sidebar Description
+
 st.sidebar.markdown("Explore and analyze crop yield data")
 
-st.write("First 5 rows of the dataset:")
-st.write(data.head())
 
-# Function to compare crops which require greater or lesser than average for a condition
+
+
+
+def display_crops_by_condition(condition, threshold, comparison_type):
+    if comparison_type == "Greater than":
+        st.write(f"Crops which require a very high ratio of {condition} content in soil:")
+        selected_crops = data[data[condition] > threshold]['label'].unique()
+    elif comparison_type == "Less than":
+        st.write(f"Crops which require a very low ratio of {condition} content in soil:")
+        selected_crops = data[data[condition] < threshold]['label'].unique()
+
+    st.table(pd.DataFrame({"Crops": selected_crops}))
+
+
 def compare_crops(condition, comparison_type):
-    if comparison_type == "Greater than average":
-        st.write(f"Crops which require greater than average {condition}:")
-        selected_crops = data[data[condition] > data[condition].mean()]['label'].unique()
-    elif comparison_type == "Less than average":
-        st.write(f"Crops which require lesser than average {condition}:")
-        selected_crops = data[data[condition] < data[condition].mean()]['label'].unique()
+    if condition != 'Select a condition':
+        if comparison_type == "Greater than average":
+            st.write(f"Crops which require greater than average {condition}:")
+            selected_crops = data[data[condition] > data[condition].mean()]['label'].unique()
+        elif comparison_type == "Less than average":
+            st.write(f"Crops which require lesser than average {condition}:")
+            selected_crops = data[data[condition] < data[condition].mean()]['label'].unique()
 
-    # Create a Pandas DataFrame to hold the comparison results
-    comparison_data = pd.DataFrame({
-        'Crops': selected_crops
-    })
+        
+        comparison_data = pd.DataFrame({
+            'Crops': selected_crops
+        })
 
-    # Display the comparison as a table
-    st.table(comparison_data)
+        
+        st.table(comparison_data)
 
-# Function to compare all crops for N, P, K, temperature, humidity, and rainfall
+
+
+condition_options = ['Select a condition', 'N', 'P', 'K', 'rainfall', 'temperature', 'humidity', 'ph']
+condition = st.sidebar.selectbox("Select Condition", condition_options, index=0)
+
+
+if condition != 'Select a condition':
+    min_threshold = int(data[condition].min())
+    max_threshold = int(data[condition].max())
+    
+    threshold = st.sidebar.slider("Select Threshold Value", min_value=min_threshold, max_value=max_threshold)
+else:
+    threshold = None
+
+
+comparison_type = st.sidebar.radio("Select Comparison Type", ['Greater than', 'Less than'])
+
+
+if condition != 'Select a condition' and threshold and comparison_type:
+    result_placeholder = st.empty()
+    result_placeholder.text("Loading... Please wait.")
+    display_crops_by_condition(condition, threshold, comparison_type)
+    result_placeholder.empty()
+
+
 def compare_all_crops():
-    condition = st.selectbox("Select Condition", ['N', 'P', 'K', 'temperature', 'humidity', 'rainfall'])
+    condition_options = ['Select a condition', 'N', 'P', 'K', 'temperature', 'humidity', 'rainfall']
+    condition = st.selectbox("Select Condition", condition_options, index=0)
     comparison_type = st.selectbox("Select Comparison Type", ['Greater than average', 'Less than average'])
     st.write(f"Comparison for {condition}:")
     compare_crops(condition, comparison_type)
 
-# Show the comparison for all conditions using the dropdown
+
 st.header("Crop Yield Analysis")
 compare_all_crops()
 
 
 
 
-# Function to display summary
 def summary(crops=None):
     if crops is None:
         crops = list(data['label'].value_counts().index)
@@ -62,7 +99,7 @@ def summary(crops=None):
     st.write(f"Summary for {crops} - Minimum, Average, and Maximum")
     st.write("--------------------------------------------------")
 
-    # Filter data for selected crops
+    
     x = data[data['label'].isin(crops)]
 
     if x.empty:
@@ -70,7 +107,7 @@ def summary(crops=None):
     else:
         z = x.drop(['label'], axis=1)
 
-        # Calculate Minimum, Average, and Maximum
+        
         summary_data = pd.DataFrame({
             'Crop': crops
         })
@@ -80,92 +117,73 @@ def summary(crops=None):
             summary_data[f'Average {i}'] = x[i].mean()
             summary_data[f'Maximum {i}'] = x[i].max()
 
-        # Display the summary as a table
+        
         st.table(summary_data)
 
-# Initialize session state with all crops as the default selection
 if 'selected_crops' not in st.session_state:
     st.session_state.selected_crops = list(data['label'].value_counts().index)
 
-# Show the selectbox to choose the crops
 st.subheader("MIN,MAX,AVG FOR SELECTED CROP")
+selected_crops_placeholder = st.empty()
 selected_crops = st.multiselect("Select Crops", data['label'].value_counts().index, st.session_state.selected_crops)
 
-# Update the session state with the selected crops
-st.session_state.selected_crops = selected_crops
 
-# Show the summary table for selected crops
-summary(selected_crops)
+if st.button("Select Crops"):
+    st.session_state.selected_crops = selected_crops
 
 
+selected_crops_placeholder.write(f"Selected Crops: {st.session_state.selected_crops}")
+summary(st.session_state.selected_crops)
 
-
-# Function to compare conditions
 def compare():
     conditions = st.radio("Select Condition", ['N', 'P', 'K', 'temperature', 'humidity', 'rainfall'])
     st.write(f"Average value for {conditions}:")
     grouped = data.groupby('label')[conditions].mean()
 
-    # Create a Pandas DataFrame to hold the comparison results
+    
     comparison_data = pd.DataFrame({
         'Crop': grouped.index,
         'Average Value': grouped.values
     })
 
-    # Display the comparison as a table
+   
     st.table(comparison_data)
 
-# Display the functions in the main content area
 st.subheader("AVERAGE CONDITION")
 compare()
-
-# Display the bar plots for each feature in the main content area
-st.header("Crop Feature Distribution")
-
-# Define the features
 features = ['N', 'P', 'K', 'temperature', 'humidity', 'ph', 'rainfall']
 
-# Create a new DataFrame for plotting
-plot_data = data[features]
 
-# Plot the histogram for each feature using Streamlit's native function
-for feature in features:
-    st.subheader(f"Histogram for {feature}")
-    st.bar_chart(plot_data[feature])
 
-# Display the elbow plot for K-Means clustering in the main content area
 st.header("Elbow Method for K-Means")
 
-# Determine Optimum number of clusters by elbow method
+
+y = data['label']
+x = data.drop(['label'], axis=1)
+
+
 wcss = []
 for i in range(1, 11):
     km = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
     km.fit(x)
     wcss.append(km.inertia_)
 
-# Plot the elbow plot using Streamlit's native function
-st.line_chart(wcss)
+
+elbow_data = pd.DataFrame({'Number of Clusters (K)': range(1, 11), 'WCSS': wcss})
+st.line_chart(elbow_data.set_index('Number of Clusters (K)'))
 
 
-# Prepare the data for model training
-y = data['label']
-x = data.drop(['label'], axis=1)
-
-# Train-test split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-# Interactive Widget for Model Selection
 st.sidebar.subheader("Model Selection")
 selected_model = st.sidebar.selectbox("Select Model", ['Logistic Regression', 'Random Forest'])
-# Train a Logistic Regression model or Random Forest model based on the selected option
-# Train a Model based on the selected option
+
+
 if selected_model == 'Logistic Regression':
     model = LogisticRegression()
-else:  # Default to Random Forest
+else:  
     model = RandomForestClassifier()
-# Fit the Model
-model.fit(x_train, y_train)
 
-# Feature Importance or Coefficients
+model.fit(x, y)
+
 st.sidebar.subheader("Model Interpretation")
 with st.sidebar.expander("Feature Importance / Coefficients"):
     if selected_model == 'Random Forest':
@@ -176,51 +194,47 @@ with st.sidebar.expander("Feature Importance / Coefficients"):
         st.write(feature_coef)
 
 
-# Display the confusion matrix and classification report in the main content area
 st.header("Model Evaluation")
 
-# Make predictions on test data
-y_predict = model.predict(x_test)
 
-# Evaluation metrics
-conf_matrix = confusion_matrix(y_test, y_predict)
+y_predict = model.predict(x)
 
-# Evaluation metrics
+conf_matrix = confusion_matrix(y, y_predict)
+
+
 evaluation_metric = st.sidebar.selectbox("Select Evaluation Metric", ['Confusion Matrix', 'Classification Report'])
 
 if evaluation_metric == 'Confusion Matrix':
-    # Display the confusion matrix in the sidebar
+    
     st.subheader("Confusion Matrix")
-    # Plot the confusion matrix
+    
     fig_confusion_matrix, ax_confusion_matrix = plt.subplots(figsize=(8, 6))
-    sns.heatmap(confusion_matrix(y_test, y_predict), annot=True, cmap="Reds", ax=ax_confusion_matrix)
+    sns.heatmap(confusion_matrix(y, y_predict), annot=True, cmap="Reds", ax=ax_confusion_matrix)
     ax_confusion_matrix.set_title("Confusion Matrix")
     ax_confusion_matrix.set_xlabel("Predicted Labels")
     ax_confusion_matrix.set_ylabel("True Labels")
     plt.tight_layout()
 
-    # Display the figure using st.pyplot() with the Matplotlib figure as an argument
+    
     st.pyplot(fig_confusion_matrix)
 
 elif evaluation_metric == 'Classification Report':
-    # Display the classification report in the sidebar
-    st.subheader("Classification Report")
-    classification_report_str = classification_report(y_test, y_predict)
-    classification_report_dict = classification_report(y_test, y_predict, output_dict=True)
 
-    # Convert the classification report dictionary to a Pandas DataFrame
+    st.subheader("Classification Report")
+    classification_report_str = classification_report(y, y_predict)
+    classification_report_dict = classification_report(y, y_predict, output_dict=True)
+
+
     classification_report_df = pd.DataFrame(classification_report_dict).transpose()
-    
-    # Display the classification report as a table
+
+
     st.table(classification_report_df)
 
 
-
-# Prediction for given climatic condition
 st.header("Crop Prediction")
 st.write("Enter the climatic conditions below and click the 'Predict' button to get the suggested crop.")
 
-# Interactive Widgets for Climatic Conditions
+
 n = st.slider("Nitrogen (N)", min_value=0, max_value=100, value=50)
 p = st.slider("Phosphorus (P)", min_value=0, max_value=100, value=50)
 k = st.slider("Potassium (K)", min_value=0, max_value=100, value=50)
@@ -229,7 +243,7 @@ humidity = st.slider("Humidity", min_value=0, max_value=100, value=60)
 ph = st.slider("pH", min_value=0, max_value=14, value=7)
 rainfall = st.slider("Rainfall", min_value=0, max_value=400, value=200)
 
-# Button to predict the crop based on the input climatic conditions
+
 if st.button("Predict"):
     prediction = model.predict([[n, p, k, temperature, humidity, ph, rainfall]])
     st.write("The suggested crop for the given climatic condition is:", prediction[0])
